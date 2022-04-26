a7b55e6ab824e205ed6b7475b5217e0dd726735b
Nick Waggoner <90349188+nswagg@users.noreply.github.com>
2022-04-25 20:35:14 -0600

    Update and rename q4.py to Nick_Waggoner_q4.py

--- More Examples/Nick_Waggoner_q4.py
+++ More Examples/Nick_Waggoner_q4.py
@@ -1,6 +1,8 @@
 import os
 import bs4
 
+# Reads from the HTML file (web-scraping) using beautifulsoup and prints out the objects matching the item type/id
+
 exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile, features="html.parser")
 span = exampleSoup.select('span[id]')
